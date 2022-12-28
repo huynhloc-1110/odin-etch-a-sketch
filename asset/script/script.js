23ce2459fecfce9ffae4356d5b857978a1466bd7
huynhloc-1110 <101441531+huynhloc-1110@users.noreply.github.com>
2022-12-28 19:19:31 +0700

    Update clear and new panel buttons function
    - Add header, footer, 2 buttons in html.
    - Add some CSS for them.
    - Put the grid generating code into a function.
    - Implement 'clear' and 'generate new panel' function for the 2 buttons.

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,21 +1,67 @@
 const drawPanel = document.querySelector('#draw-panel');
-let edge = 16;
+let pixels;
+generateGrid(16);
 
-for (let i = 0; i < edge*edge; i++) {
-  const pixel = document.createElement('div');
-  pixel.classList.add('grid-item');
-  pixel.setAttribute('draggable', 'false');
-  pixel.addEventListener('mousedown', draw);
-  pixel.addEventListener('mouseover', draw);
-  drawPanel.appendChild(pixel);
+const btnClear = document.querySelector('#btn-clear');
+btnClear.addEventListener('click', clear);
+
+const btnNewPanel = document.querySelector('#btn-new-panel');
+btnNewPanel.addEventListener('click', generateNewGrid);
+
+function generateGrid(edgePixel) {
+  pixels = [];
+  for (let i = 0; i < edgePixel*edgePixel; i++) {
+    const pixel = document.createElement('div');
+    pixels.push(pixel);
+    pixel.classList.add('grid-item');
+    pixel.setAttribute('draggable', 'false');
+    pixel.addEventListener('mousedown', draw);
+    pixel.addEventListener('mouseover', draw);
+    drawPanel.appendChild(pixel);
+  }
 }
 
 function draw(mouseEvent) {
   /**
-   * because dragging and selecting is the default behaviors on
-   * mousedown events, we needs to disable it first.
+   * Because dragging and selecting is the default behaviors on mousedown
+   * events, we needs to disable it first.
    */
   mouseEvent.preventDefault();
+
+  /**
+   * This is for mouseover event when the user hold the mouse through the pixel.
+   */
   if (mouseEvent.buttons == 1)
     this.style.backgroundColor = "black";
+}
+
+function clear(event) {
+  for (const pixel of pixels) {
+    pixel.removeAttribute('style');
+  }
+}
+
+function generateNewGrid() {
+  /**
+   * Input and validate edge pixel, return if edge pixel > 100
+   * because 100x100 div will cause lagging.
+   */
+  let edgePixel = prompt("Enter n as edge pixel" +
+    " (this action will generate new n x n panel):");
+  if (edgePixel > 100) {
+    alert("n is over 100!");
+    return;
+  }
+  
+  /**
+   * Delete the old grid, set css for new grid, generate new grid.
+   * There is another way to set the grid columns and rows for the new grid;
+   * however, it uses css var, which I haven't known yet.
+   */
+  drawPanel.innerHTML = "";
+  drawPanel.style.setProperty('grid-template-columns',
+    `repeat(${edgePixel}, 1fr)`);
+  drawPanel.style.setProperty('grid-template-rows',
+    `repeat(${edgePixel}, 1fr)`);
+  generateGrid(edgePixel);
 }
